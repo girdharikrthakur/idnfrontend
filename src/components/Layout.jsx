6ed4b1f56c655aa79b138502713963032a408753
Girdhari Thakur <girdharikumar101@gmail.com>
2026-04-26 15:54:24 +0530

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
-import Sidebar from "./SideBar.jsx";
-import Navbar from "./Navbar.jsx";
 import { Outlet } from "react-router-dom";
-import Footer from "./Footer.jsx";
+
+// Components
+import Navbar from "./NavBar.jsx";
 import SecNavBar from "./SecNavBar.jsx";
+import Sidebar from "./SideBar.jsx";
+import Footer from "./Footer.jsx";
 
-function Layout() {
-  const [isOpen, setIsOpen] = useState(false);
+const Layout = () => {
+  // 1. More descriptive state variable names
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+  // 2. Safer state update using the previous state (prev)
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar toggle={toggle} />
-      <SecNavBar />
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <div className="flex-1">
+    <div className="flex flex-col min-h-screen">
+      {/* 3. Grouping navigation elements inside a semantic <header> */}
+      <header>
+        <Navbar toggle={toggleSidebar} />
+        <SecNavBar />
+      </header>
+
+      <Sidebar isOpen={isSidebarOpen} toggle={toggleSidebar} />
+
+      {/* 4. Using the semantic <main> tag instead of a <div> */}
+      <main className="flex-1">
         <Outlet />
-      </div>
+      </main>
+
       <Footer />
     </div>
   );
-}
+};
 
 export default Layout;
